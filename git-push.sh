bb71a1a99b6b93d6f81897643b947f924f1207a9
yongjusong <yongjusyj@gmail.com>
2020-04-22 19:50:04 +0900

    modify

--- git-push.sh
+++ git-push.sh
@@ -1,9 +1,3 @@
-#!/bin/sh
-repo_name=$1
-test -z $repo_name && echo "Repo name required." 1>&2 && exit 1
-curl -u '[username]:[password]' https://api.github.com/user/repos -d "{\"name\":\"$repo_name\"}"
-git init
 git add .
-git commit -m "initial commit"
-git remote add origin "https://github.com/yongjusong/$repo_name.git"
+git commit -m "$1"
 git push -u origin master
